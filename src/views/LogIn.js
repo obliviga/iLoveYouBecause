53a16350810427979222dcf799601422eec8608f
Anish Kshatriya <obliviga@gmail.com>
2018-06-28 18:10:00 -0700

    styling login view. using material SnackBar component to show error message

--- src/views/LogIn.js
+++ src/views/LogIn.js
@@ -2,12 +2,21 @@ import React, { Component } from 'react';
 import { Link, withRouter } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import Paper from '@material-ui/core/Paper';
+import Snackbar from '@material-ui/core/Snackbar';
+import IconButton from '@material-ui/core/IconButton';
+import ErrorIcon from '@material-ui/icons/Error';
+import CloseIcon from '@material-ui/icons/Close';
+import { EmoticonSad } from 'mdi-material-ui'
+
 import byPropKey from '../utils/byPropKey';
 import { auth } from '../firebase';
 import * as routes from '../constants/routes';
 import Input from '../components/Input/Input';
 import PasswordInput from '../components/Input/PasswordInput';
 import Button from '../components/Button/Button';
+
+import './LogIn.css';
 
 class LogIn extends Component {
   constructor(props) {
@@ -21,6 +30,7 @@ class LogIn extends Component {
       email: '',
       password: '',
       error: null,
+      open: false,
     };
   }
 
@@ -44,7 +54,10 @@ class LogIn extends Component {
         history.push(routes.DASHBOARD);
       })
       .catch(error => {
-        this.setState(byPropKey('error', error));
+        this.setState({
+          open: true,
+          error,
+        });
       });
 
     event.preventDefault();
@@ -58,6 +71,18 @@ class LogIn extends Component {
     this.setState(byPropKey('password', event.target.value));
   }
 
+  handleClick = () => {
+    this.setState({ open: true });
+  };
+
+  handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    this.setState({ open: false });
+  };
+
   render() {
     const {
       email,
@@ -68,32 +93,75 @@ class LogIn extends Component {
     const isInvalid = password === '' || email === '';
 
     return (
-      <div>
-        <h1>Log In</h1>
-        <form onSubmit={this.handleOnSubmit}>
-          <Input
-            value={email}
-            onChange={this.handleOnChangeEmail}
-            type="email"
-            placeholder="Email Address"
-          />
+      <div className="container">
+        <h1>iLoveYouBecause</h1>
+        <div className="paperContainer">
+          <Paper className="paper">
+            <form onSubmit={this.handleOnSubmit}>
+              <Input
+                value={email}
+                onChange={this.handleOnChangeEmail}
+                type="email"
+                placeholder="Jyn.Erso@rogue1.com"
+                label="Email Address"
+              />
 
-          <PasswordInput
-            value={password}
-            onChange={this.handleOnChangePassword}
-          />
-
-          <Button
-            type="submit"
-            disabled={isInvalid}
-            text="Log In"
-          />
-
-          { error && <p>{error.message}</p> }
-        </form>
-        <Link to="/pw-forget">Forgot Password?</Link>
-        <p>Don&apos;t have an account?</p>
-        <Link to={routes.SIGN_UP}>Sign Up</Link>
+              <div className="passwordInput">
+                <PasswordInput
+                  value={password}
+                  onChange={this.handleOnChangePassword}
+                />
+              </div>
+              <div className="buttonContainer">
+                <Button
+                  type="submit"
+                  disabled={isInvalid}
+                  text="Log In"
+                  size="large"
+                />
+              </div>
+            </form>
+            <div className="formText">
+              <Link
+                className="forgotPasswordLink"
+                to="/pw-forget"
+              >
+                Forgot Password?
+              </Link>
+              <div className="signUpContainer">
+                <p>Don&apos;t have an account?</p>
+                <Link to={routes.SIGN_UP}> Sign Up!</Link>
+              </div>
+            </div>
+          </Paper>
+        </div>
+        <Snackbar
+          className="snackbar"
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'left',
+          }}
+          open={this.state.open}
+          autoHideDuration={60000000}
+          onClose={this.handleClose}
+          ContentProps={{
+            'aria-describedby': 'message-id',
+          }}
+          message={
+            <span id="message-id" className="messageContainer">
+              <EmoticonSad />
+              <p className="message">{error && error.message}</p>
+              <IconButton
+                key="close"
+                aria-label="Close"
+                color="inherit"
+                onClick={this.handleClose}
+              >
+                <CloseIcon />
+              </IconButton>
+            </span>
+          }
+        />
       </div>
     );
   }
