248823e49925554b707a6d4e732f2974e61d3f90
Anish Kshatriya <obliviga@gmail.com>
2018-06-28 18:08:25 -0700

    adding size prop for button component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,13 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MaterialButton from '@material-ui/core/Button';
 
-const Button = ({ text, onClick, type, disabled }) => (
+const Button = ({ text, onClick, type, disabled, size }) => (
   <MaterialButton
     variant="contained"
     color="primary"
     type={type}
     onClick={onClick}
     disabled={disabled}
+    size={size}
   >
     {text}
   </MaterialButton>
@@ -19,12 +20,14 @@ Button.propTypes = {
   text: PropTypes.string.isRequired,
   onClick: PropTypes.func,
   disabled: PropTypes.bool,
+  size: PropTypes.string,
 };
 
 Button.defaultProps = {
   type: 'button',
   disabled: false,
   onClick: () => {},
+  size: 'normal',
 };
 
 export default Button;
