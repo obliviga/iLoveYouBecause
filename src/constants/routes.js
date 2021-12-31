206219bb4c14a0e5ac62bc0c6d94fb190b3088fe
Anish Kshatriya <obliviga@gmail.com>
2021-12-31 12:31:43 -0800

    Install fresh project via create-react-app

--- src/constants/routes.js
+++
@@ -1,8 +0,0 @@
-export const SIGN_UP = '/signup';
-export const LOG_IN = '/login';
-export const DASHBOARD = '/';
-export const SETTINGS = '/settings';
-export const PASSWORD_FORGET = '/pw-forget';
-export const LOVEDONES = '/lovedOnes';
-export const LOVEDONEPROFILE = '/lovedOne';
-export const ARCHIVEDREASONS = '/archive';
--- src/utils/byPropKey.js
+++
@@ -1,5 +0,0 @@
-const byPropKey = (propertyName, value) => () => ({
-  [propertyName]: value,
-});
-
-export default byPropKey;
